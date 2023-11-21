e2b451a176e9f76851e25997ca1c24a800db3cc5
es-xccc <r2605572@gmail.com>
2023-11-21 19:07:31 +0800

    ex5

--- ex5/ReadTemperatureFile.py
+++ ex5/ReadTemperatureFile.py
@@ -58,10 +58,10 @@ for city_data in data:
             year_sum += city_data[j+1][i+1]
         mon_avg_temp.append(round(year_sum/10, 2))
     allavg = round(sum(mon_avg_temp)/12, 2)
-    plt.axhline(y=allavg, color='r', linestyle='--')
+    plt.axhline(y=allavg, color='r', linestyle='--', label="Mean of 10 Years")
     plt.text(1, allavg, str(allavg), fontsize=8)
     plt.plot(x, mon_avg_temp, 'ro')
-    plt.plot(x, mon_avg_temp, label="Mean of 10 Years")
+    plt.plot(x, mon_avg_temp)
     for i in range(len(x)):
         plt.text(x[i], mon_avg_temp[i], str(mon_avg_temp[i]), fontsize=8)
     plt.xticks(range(1, 13))
@@ -70,4 +70,24 @@ for city_data in data:
     plt.xlabel('Month')
     plt.ylabel('Temperature in Degree C')
     plt.savefig(city_name[itr%3] + " Monthly Mean Temperature Of 2013 To 2022" + ".png")
+    itr += 1
+
+for city_data in data:
+    plt.figure(itr)
+    x = list(range(2013, 2023))
+    year_sum = 0
+    for i in range(12):
+        year_list = []
+        for j in range(10):
+            year_list.append(city_data[j+1][i+1])
+            year_sum += city_data[j+1][i+1]
+        plt.plot(x, year_list, label="Mean of " + str(i+1) + "th Month")
+    plt.axhline(y=year_sum/120, color='r', linestyle='--', label="Mean of the Years")
+    plt.text(2013, round(year_sum/120, 2), str(round(year_sum/120, 2)))
+    plt.xticks(range(2013, 2023))
+    plt.legend(loc='lower right', prop={'size': 6})
+    plt.title(city_name[itr%3] + " Mean Temperature Of Month and Total Year Mean 2013 To 2022")
+    plt.xlabel('Years')
+    plt.ylabel('Temperature in Degree C')
+    plt.savefig(city_name[itr%3] + " Mean Temperature Of Month and Total Year Mean 2013 To 2022" + ".png")
     itr += 1
